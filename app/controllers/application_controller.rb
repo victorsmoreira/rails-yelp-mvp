b447db7e853de7265357b1a0bbf6b352485ea71c
Victor Moreira <victorsmoreira@outlook.com>
2021-08-18 15:34:50 -0300

    rating form embedded on restaurant page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,3 @@
 class ApplicationController < ActionController::Base
+  Rack::MiniProfiler.config.position = 'bottom-left'
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,19 +1,11 @@
 class ReviewsController < ApplicationController
-  def new
-    @restaurant = Restaurant.find(params[:restaurant_id])
-    @review = Review.new
-  end
-
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
-    if @review.valid?
-      @review.save
-      redirect_to restaurant_path(@restaurant)
-    else
-      render :new
-    end
+    @review.save if @review.valid?
+
+    redirect_to restaurant_path(@restaurant)
   end
 
   private

9c0a2918ba0750850e9cdf3cacfbbd937b1e43d8
Victor Moreira <victorsmoreira@outlook.com>
2021-08-18 14:53:53 -0300

    MVP done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  name = Faker::Restaurant.name
+  address = Faker::Address.full_address
+  category = Restaurant::CATEGORY.sample
+  restaurant = Restaurant.new(name: name, address: address, category: category)
+  restaurant.save
+end
